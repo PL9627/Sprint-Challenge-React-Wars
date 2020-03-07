7d2268e8cd39f3cae482ee61379d776ae33f6522
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-03-06 17:22:55 -0800

    added styled components

--- src/components/CharaCard.js
+++ src/components/CharaCard.js
@@ -1,11 +1,23 @@
 import React from "react";
+import styled from 'styled-components';
+
+const Card = styled.div`
+  display: flex;
+  justify-content: center;
+  border: 1px solid black;
+  width: 30%;
+  margin: 0 0 2% 35%;
+  background: gray;
+`;
 
 const CharaCard = (props) => {
   return (
-      <div>
-        <p>{props.name}</p>
-        <p>{props.birth_year}</p>
-      </div>
+      <Card>
+        <div>
+          <p>{props.name}</p>
+          <p>{props.birth_year}</p>
+        </div>
+      </Card>
   );
 };
 
